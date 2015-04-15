51e63d8800cf3b6879326e95ab92b0a521f5bc0d
Jonatas Leon <jonatas.leon@gmail.com>
2015-04-15 01:45:44 -0300

    Lista 02 - Exerc 01 Optimized again

--- Lista 02/Exerc01.java
+++ Lista 02/Exerc01.java
@@ -5,21 +5,14 @@ public class Exerc01 {
 		Scanner read = new Scanner(System.in);
 		Scanner readStr = new Scanner(System.in);
 
-		do {
-			double n1, n2;
-			n1 = n2 = -1;
+		double n1, n2;
 
-			for(int i = 0; (n1 < 0 || n1 > 10); i++) {
-				if(i != 0) System.out.print("Nota invalida: ");
-				System.out.print("Digite a primeira nota: ");
-				n1 = read.nextDouble();
-			}
-		
-			for(int i = 0; (n2 < 0 || n2 > 10); i++) {
-				if(i != 0) System.out.print("Nota invalida: ");
-				System.out.print("Digite a segunda nota: ");
-				n2 = read.nextDouble();
-			}
+		do {
+			System.out.print("Digite a primeira nota: ");
+			while((n1 = read.nextDouble()) < 0 || n1 > 10) System.out.print("Erro: Digite uma nota entra 0 e 10: ");
+	
+			System.out.print("Digite a segunda nota: ");
+			while((n2 = read.nextDouble()) < 0 || n2 > 10) System.out.print("Erro: Digite uma nota entre 0 e 10: ");
 
 			System.out.printf("Média final: %2.2f\n", (n1 + n2) / 2);
 
